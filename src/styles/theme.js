ae9ae29d8f4f4ef025ce1797a40f47131282c596
Danielle da Luz <daniln2209@gmail.com>
2022-11-30 17:58:04 +0000

    feat: add theme object with properties

---
+++ src/styles/theme.js
@@ -0,0 +1,26 @@
+export const theme = {
+    colors: {
+        primary: {
+            primary: "#FF577F",
+            primaryFocus: "#FF427F",
+            primaryNegative: "#59323F"
+        },
+
+        status: {
+            sucess: "#3FE864",
+            negative: "#E83F5B"
+        },
+
+        greyScale: ["#F8F9FA", "#868E96", "#343B41", "#212529", "#121214"]
+    },
+
+    weight: {
+        bold: 700,
+        medium: 600,
+        regular: 400
+    },
+
+    fontSizes: [18, 16, 14, 12],
+
+    radius: 4
+}
