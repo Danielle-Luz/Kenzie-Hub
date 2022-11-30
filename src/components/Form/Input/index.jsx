796ed3fc99b79ed76f21c228ec32fb96c970ce79
Danielle da Luz <daniln2209@gmail.com>
2022-11-30 19:03:36 +0000

    feat: add input components

---
+++ src/components/Form/Input/index.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Input = ({placeholder, name, options}) => {
+    
+}
+
+export const InputWrapper = ({label, placeholder, name, options}) => {
+
+}
