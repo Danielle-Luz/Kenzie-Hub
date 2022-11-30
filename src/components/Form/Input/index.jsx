2a993c3b7e190a2dcca978e05067a7b70a577efc
Danielle da Luz <daniln2209@gmail.com>
2022-11-30 20:44:50 +0000

    feat: import input on input styles file

--- src/components/Form/Input/index.jsx
+++ src/components/Form/Input/index.jsx
@@ -1,7 +1,14 @@
 import styled from "styled-components";
 
 export const Input = ({placeholder, name, options}) => {
-  
+  return (
+    options ? 
+      <select>
+        
+      </select>
+    :
+    <input placeholder={placeholder} />
+  );
 }
 
 export const InputWrapper = ({label, placeholder, name, options}) => {
--- src/components/Form/Input/styles.js
+++ src/components/Form/Input/styles.js
@@ -1,30 +1,36 @@
 import styled from "styled-components";
+import { Input } from ".";
+
+export const InputStyled = styled(Input)`
+  background-color: ${({ theme }) => theme.colors.greyScale[2]};
+
+  border: 1px solid transparent;
+  border-radius: ${({ theme }) => theme.radius};
+
+  color: ${({ theme }) => theme.colors.greyScale[0]};
+
+  font-size: ${({ theme }) => theme.fontSizes[1]}px;
+
+  padding: 10.5px 16.2px;
+
+  width: 100%;
+
+  &:focus {
+    border-color: ${({ theme }) => theme.colors.greyScale[0]};
+  }
+
+  &:placeholder {
+    color: ${({ theme }) => theme.colors.greyScale[1]};
+  }
+`;
 
 export const InputWrapper = styled.article`
   label {
     margin-bottom: 22px;
   }
 
-  input {
-    background-color: ${({theme}) => theme.colors.greyScale[2]};
-
-    border: 1px solid transparent;
-    border-radius: ${({theme}) => theme.radius};
-
-    color: ${({theme}) => theme.colors.greyScale[0]};
-
-    font-size: ${({theme}) => theme.fontSizes[1]}px;
-
-    padding: 10.5px 16.2px;
-
-    width: 100%;
-    
-    &:focus {
-      border-color: ${({theme}) => theme.colors.greyScale[0]};
-    }
-
-    &:placeholder {
-      color: ${({theme}) => theme.colors.greyScale[1]};
-    }
+  option {
+    background-color: ${({ theme }) => theme.colors.greyScale[2]};
+    color: ${({ theme }) => theme.colors.greyScale[0]};
   }
-`
+`;
