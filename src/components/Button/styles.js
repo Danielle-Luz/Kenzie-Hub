06fbca0ad86b5b11994706ecceb97c25c41a5c91
Axionauta <daniln2209@gmail.com>
2022-11-30 20:46:07 -0300

    feat: add button terciary styles

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -15,3 +15,13 @@ export const ButtonPrimary = styled(Button)`
     background-color: ${({ theme }) => theme.colors.primary.focus};
   }
 `;
+
+export const ButtonTerciary = styled(Button)`
+  background-color: ${({ theme }) => theme.colors.greyScale[3]};
+  font-size: ${({ theme }) => theme.fontSizes[3]};
+  padding: 8px 16px;
+
+  &:focus {
+    background-color: ${({ theme }) => theme.colors.greyScale[2]};
+  }
+`;
