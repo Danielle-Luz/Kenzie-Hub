6f0c3c3739f45102e225d906dc3ed1006b0a1369
Danielle da Luz <daniln2209@gmail.com>
2022-11-30 19:47:52 +0000

    feat: separate text from title components

--- src/components/fonts/Text/styles.js
+++ src/components/fonts/Text/styles.js
@@ -3,22 +3,8 @@ import styled, { css } from "styled-components";
 import { Text } from ".";
 
 const textTypes = {
-  h1: css`
-    font-size: ${({ theme }) => theme.fontSizes[0]}px;
-    font-weight: ${({theme}) => theme.weight.bold}
-  `,
-
-  h1: css`
-    font-size: ${({ theme }) => theme.fontSizes[1]}px;
-    font-weight: ${({theme}) => theme.weight.bold}
-  `,
-
   label: css`
     font-size: ${({ theme }) => theme.fontSizes[3]}px;
-  `,
-
-  h2: css`
-  
   `,
 
   span: css`
