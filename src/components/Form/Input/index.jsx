3320e328c0000c5b5b694927fa9d9843dba1cfcc
Danielle da Luz <daniln2209@gmail.com>
2022-11-30 20:40:53 +0000

    feat: add input style

--- src/components/Form/Input/index.jsx
+++ src/components/Form/Input/index.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Input = ({placeholder, name, options}) => {
-    
+  
 }
 
 export const InputWrapper = ({label, placeholder, name, options}) => {
