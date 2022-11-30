607aff8f80f15f8dbdbc7039be9654cf241728cf
Danielle da Luz <daniln2209@gmail.com>
2022-11-30 20:19:12 +0000

    feat: import font

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -3,6 +3,7 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyles = createGlobalStyle`
     * {
         box-sizing: border-box;
+        font-family: 'Inter', sans-serif;
         margin: 0;
         padding: 0;
     }
