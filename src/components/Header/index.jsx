496d9b24f85bd75e604b7079c2a0212799955eb9
Axionauta <daniln2209@gmail.com>
2022-11-30 22:44:28 -0300

    feat: create header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,2 +1,14 @@
 import tinyLogo from "../../assets/imgs/logo-tiny.svg";
 import bigLogo from "../../assets/imgs/logo-big.svg";
+import { ButtonSecondary } from "../Button/Secondary";
+
+export function Header ({hasTinyLogo, onClick}) {
+  return (
+    <header>
+      <img src={hasTinyLogo ? tinyLogo : bigLogo} alt="logo" />
+      {
+        onClick && <ButtonSecondary onClick={onClick} />
+      }
+    </header>
+  );
+}
