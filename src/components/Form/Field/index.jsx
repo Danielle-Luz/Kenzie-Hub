640b94b63c75af472f76d5b4a5bbb0f811816268
Axionauta <daniln2209@gmail.com>
2022-12-01 14:52:48 -0300

    feat: add error message on input

--- src/components/Form/Field/index.jsx
+++ src/components/Form/Field/index.jsx
@@ -1,15 +1,19 @@
 import { FieldStyled, FieldWrapperStyled } from "./styles";
 import { TextStyled } from "../../fonts/Text/styles";
 
-export function Field({ className, name, options, placeholder }) {
-  return options ? (
-    <select className={className} defaultValue={placeholder}>
-      {options.map((option) => (
-        <option>{option}</option>
-      ))}
-    </select>
-  ) : (
-    <input className={className} placeholder={placeholder} />
+export function Field({ className, name, options, placeholder, error }) {
+  return (
+    <>
+      (options ?
+      <select className={className} defaultValue={placeholder}>
+        {options.map((option) => (
+          <option>{option}</option>
+        ))}
+      </select>
+      :
+      <input className={className} placeholder={placeholder} />)
+      {error && <TextStyled tag="span">{error}</TextStyled>}
+    </>
   );
 }
 
