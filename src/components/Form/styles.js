c9d29059e5409c369261eac243f78f5de413e83f
Axionauta <daniln2209@gmail.com>
2022-12-01 01:55:36 -0300

    feat: add form text inside main

--- src/components/Form/styles.js
+++ src/components/Form/styles.js
@@ -4,17 +4,11 @@ const formFlexSpacing = css`
   display: flex;
   flex-direction: column;
   gap: 22px;
-`
+`;
 
 export const FormStyled = styled.form`
-  background-color: ${({theme}) => theme.colors.greyScale[3]};
-
-  border-radius: ${({theme}) => theme.radius}px;
-
-  padding: 42px 22px;
-
   ${formFlexSpacing}
-`
+`;
 
 export const FormContainer = styled.section`
   max-width: 370px;
@@ -25,6 +19,16 @@ export const FormContainer = styled.section`
   }
 
   main {
+    background-color: ${({ theme }) => theme.colors.greyScale[3]};
+
+    border-radius: ${({ theme }) => theme.radius}px;
+
+    padding: 42px 22px;
+
     ${formFlexSpacing}
+
+    h2, span {
+      text-align: center;
+    }
   }
-`
+`;
