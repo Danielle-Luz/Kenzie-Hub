813f44e46bad9d92ae4c138439a5e8d03a8cd187
Axionauta <daniln2209@gmail.com>
2022-12-01 19:55:47 -0300

    feat: create login component jsx

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,11 +1,17 @@
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { loginSchema } from "./loginSchema";
+import { fieldsList } from "./loginFormFieldsList";
 import { Form } from "../../components/Form";
 import { toast } from "react-toastify";
 import { api } from "../../services/api";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import { FormContainer } from "../../components/Form/styles";
+import { Header } from "../../components/Header";
+import { TitleStyled } from "../../components/fonts/Title/styles";
+import { TextStyled } from "../../components/fonts/Text/styles";
+import { ButtonSecondary } from "../../components/Button/Terciary";
 
 export function Login() {
   const navigate = useNavigate();
@@ -40,4 +46,24 @@ export function Login() {
       }
     }
   }
+
+  return (
+    <FormContainer>
+      <Header />
+      <main>
+        <TitleStyled tag="h2">Login</TitleStyled>
+        <Form
+          button
+          buttonText="Entrar"
+          errors={errors}
+          handleSubmit={handleSubmit}
+          submitData={login}
+          fieldsList={fieldsList}
+          register={register}
+        />
+      </main>
+      <TextStyled tag="span">Ainda não possui uma conta?</TextStyled>
+      <ButtonSecondary to="/register">Cadastre-se</ButtonSecondary>
+    </FormContainer>
+  );
 }
