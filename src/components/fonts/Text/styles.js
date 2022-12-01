6f3e6de7a65afc52ef483ce177c102529fa97cb0
Axionauta <daniln2209@gmail.com>
2022-12-01 14:54:57 -0300

    feat: add color prop on input

--- src/components/fonts/Text/styles.js
+++ src/components/fonts/Text/styles.js
@@ -13,8 +13,11 @@ const textTypes = {
 };
 
 export const TextStyled = styled(Text)`
-  font-size: ${({ theme }) => theme.fontSizes[3]}px;
+  ${({color}) => {
+    color && css`color: ${color};`
+  }}
 
+  font-size: ${({ theme }) => theme.fontSizes[3]}px;
   font-weight: ${({ theme, weight }) =>
     weight ? theme.weight[weight] : theme.weight.regular};
 
