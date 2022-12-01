e36c0818040ae15fdaac11e44bd013ec0fda0185
Axionauta <daniln2209@gmail.com>
2022-12-01 19:20:15 -0300

    feat: create field list for login page

---
+++ src/pages/Login/loginFormFieldsList.js
@@ -0,0 +1,14 @@
+export const fieldsList = [
+  {
+    label: "Email",
+    placeholder: "Digite aqui seu email",
+    name: "email",
+    type: "email"
+  },
+  {
+    label: "Senha",
+    placeholder: "Digite aqui sua senha",
+    name: "password",
+    type: "password"
+  },
+];
