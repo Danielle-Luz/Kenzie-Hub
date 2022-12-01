8369fe81a036945933fd825c4b61e59f0883ed23
Axionauta <daniln2209@gmail.com>
2022-12-01 00:47:46 -0300

    feat: add field rendering

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,9 +1,19 @@
 import { FormStyled } from "./styles";
+import { FieldWrapper } from "./Field";
 
-export function Form ({fieldsList, onSubmit}) {
+export function Form({ fieldsList, onSubmit }) {
   return (
     <FormStyled onSubmit={onSubmit}>
-
+      {fieldsList.map(({ label, placeholder, name, options }) => {
+        return (
+          <FieldWrapper
+            label={label}
+            placeholder={placeholder}
+            name={name}
+            options={options}
+          />
+        );
+      })}
     </FormStyled>
   );
 }
