0eb02e469e0d4a1b3fc7c90be3e113f5373fd195
Axionauta <daniln2209@gmail.com>
2022-11-30 21:03:58 -0300

    feat: add button icon style

--- src/components/Button/Secondary/styles.js
+++ src/components/Button/Secondary/styles.js
@@ -4,8 +4,14 @@ import { Button } from "../styles";
 
 export const ButtonSecondaryStyled = styled(Button)`
   background-color: ${({ theme }) => theme.colors.greyScale[3]};
+
+  align-items: center;
+  display: flex;
+  justify-content: center;
+
   font-size: ${({ theme }) => theme.fontSizes[3]};
-  padding: 8px 16px;
+
+  padding: 12px ${({ icon }) => !icon && "17px"};
 
   &:focus {
     background-color: ${({ theme }) => theme.colors.greyScale[2]};
