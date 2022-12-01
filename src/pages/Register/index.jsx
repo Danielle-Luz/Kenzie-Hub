0518aee9d3ee005b7b3c486ccca5bc804eb583e9
Axionauta <daniln2209@gmail.com>
2022-12-01 15:05:04 -0300

    feat: add file for fields data

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -5,53 +5,11 @@ import { TextStyled } from "../../components/fonts/Text/styles";
 import { Header } from "../../components/Header";
 import { ButtonPrimary } from "../../components/Button/Primary";
 import { api } from "../../services/api";
+import { useForm } from "react-hook-form";
+import { fieldsList } from "./registerFormFieldsList";
 
 export function Register() {
-  const fieldsList = [
-    {
-      label: "Nome",
-      placeholder: "Digite aqui seu nome",
-      name: "name",
-    },
-    {
-      label: "Email",
-      placeholder: "Digite aqui seu email",
-      name: "email",
-    },
-    {
-      label: "Senha",
-      placeholder: "Digite aqui sua senha",
-      name: "password",
-    },
-    {
-      label: "Confirmar senha",
-      placeholder: "Digite novamente sua senha",
-      name: "confirmPassword",
-    },
-    {
-      label: "Bio",
-      placeholder: "Fale sobre você",
-      name: "bio",
-    },
-    {
-      label: "Contato",
-      placeholder: "Opção de contato",
-      name: "contact",
-    },
-    {
-      label: "Selecionar módulo",
-      name: "module",
-      placeholder: "Primeiro módulo",
-      options: [
-        "Primeiro módulo",
-        "Segundo módulo",
-        "Terceiro módulo",
-        "Quarto módulo",
-        "Quinto módulo",
-        "Sexto módulo",
-      ],
-    },
-  ];
+  
 
   async function createUser(data) {
     try {
---
+++ src/pages/Register/registerFormFieldsList.js
@@ -0,0 +1,45 @@
+export const fieldsList = [
+  {
+    label: "Nome",
+    placeholder: "Digite aqui seu nome",
+    name: "name",
+  },
+  {
+    label: "Email",
+    placeholder: "Digite aqui seu email",
+    name: "email",
+  },
+  {
+    label: "Senha",
+    placeholder: "Digite aqui sua senha",
+    name: "password",
+  },
+  {
+    label: "Confirmar senha",
+    placeholder: "Digite novamente sua senha",
+    name: "confirmPassword",
+  },
+  {
+    label: "Bio",
+    placeholder: "Fale sobre você",
+    name: "bio",
+  },
+  {
+    label: "Contato",
+    placeholder: "Opção de contato",
+    name: "contact",
+  },
+  {
+    label: "Selecionar módulo",
+    name: "module",
+    placeholder: "Primeiro módulo",
+    options: [
+      "Primeiro módulo",
+      "Segundo módulo",
+      "Terceiro módulo",
+      "Quarto módulo",
+      "Quinto módulo",
+      "Sexto módulo",
+    ],
+  },
+];
