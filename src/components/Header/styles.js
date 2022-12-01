e016798b23d000d5f6af3db32f46f637f90b58c9
Axionauta <daniln2209@gmail.com>
2022-12-01 00:20:21 -0300

    feat: add header styled component

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,1 +1,8 @@
-  
+  import styled from "styled-components";
+
+  export const HeaderStyled = styled.header`
+    align-items: center;
+    display: flex;
+    justify-content: ${({buttonText}) => buttonText ? "space-between" : "center"};
+    width: 100%;
+  `
