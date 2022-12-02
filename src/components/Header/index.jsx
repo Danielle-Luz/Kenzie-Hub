f8241a529feb98e96d9218cb0472cc7eb16175a6
Axionauta <daniln2209@gmail.com>
2022-12-01 23:31:18 -0300

    feat: change header button

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,13 @@
 import tinyLogo from "../../assets/imgs/logo-tiny.svg";
 import bigLogo from "../../assets/imgs/logo-big.svg";
-
-import { ButtonSecondary } from "../Button/Secondary";
+import { ButtonTerciary } from "../Button/Terciary";
 import { HeaderStyled } from "./styles";
 
 export function Header({ hasTinyLogo, buttonText, to, darker }) {
   return (
     <HeaderStyled buttonText={buttonText}>
       <img src={hasTinyLogo ? tinyLogo : bigLogo} alt="logo" />
-      {buttonText && <ButtonSecondary to={to} darker>{buttonText}</ButtonSecondary>}
+      {buttonText && <ButtonTerciary to={to} darker>{buttonText}</ButtonTerciary>}
     </HeaderStyled>
   );
 }
