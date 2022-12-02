69a50462aee8504032e7a3c761b3060720bea450
Axionauta <daniln2209@gmail.com>
2022-12-01 21:33:58 -0300

    feat: create get user data function on user effect

---
+++ src/pages/Dashboard/index.jsx
@@ -0,0 +1,29 @@
+import { UserInfo } from "./UserInfo";
+import { Header } from "../../components/Header";
+import { Container } from "./styles";
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
+
+export function Dashboard() {
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    const userId = localStorage.getItem("@USERID");
+
+    async function getUserData() {
+      try {
+        const foundUserData = (await api.get(`users/${userId}`)).data;
+
+        setUserData(foundUserData);
+      } catch (err) {}
+    }
+
+    getUserData();
+  }, []);
+
+  return (
+    <>
+      
+    </>
+  );
+}
