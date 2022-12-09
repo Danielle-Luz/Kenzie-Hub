b522549711939fbebb8a0e2e3ad87b52ae5dbe49
Axionauta <daniln2209@gmail.com>
2022-12-09 18:05:18 -0300

    feat: create technologies component

---
+++ src/pages/Dashboard/Technologies/index.jsx
@@ -0,0 +1,16 @@
+import { TechHeaderStyled } from "./styles";
+import { Container } from "../styles";
+import { TitleStyled } from "../../../components/fonts/Title/styles";
+import { TextStyled } from "../../../components/fonts/Text/styles";
+import { ButtonSecondary } from "../../../components/Button/Secondary";
+
+export function Technologies() {
+  return (
+    <Container>
+      <TechHeaderStyled>
+        <TitleStyled tag="h2">Tecnologias</TitleStyled>
+        <ButtonSecondary button>+</ButtonSecondary>
+      </TechHeaderStyled>
+    </Container>
+  );
+}
--- src/pages/Dashboard/Technologies/styles.js
+++ src/pages/Dashboard/Technologies/styles.js
@@ -1,5 +1,8 @@
 import styled from "styled-components";
+import { Container } from "../styles";
 
 export const TechHeaderStyled = styled.article`
-
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
 `
