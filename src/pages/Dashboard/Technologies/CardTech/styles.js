8a5ed252cc11ec09a78de12f6448fe49e3f62490
Axionauta <daniln2209@gmail.com>
2022-12-10 00:55:22 -0300

    feat: add card tech style

---
+++ src/pages/Dashboard/Technologies/CardTech/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const CardTechStyled = styled.li`
+  align-items: center;
+  background-color: ${({theme}) => theme.colors.greyScale[4]};
+  border-radius: ${({theme}) => theme.radius};
+  display: flex;
+  justify-content: space-between;
+  padding: 14px 22px;
+
+  &:hover {
+    background-color: ${({theme}) => theme.colors.greyScale[2]};
+  }
+`
