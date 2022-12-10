101ba135364a0a14583d41a80c8d7069adfede52
Axionauta <daniln2209@gmail.com>
2022-12-10 11:55:39 -0300

    feat: add show modal event

--- src/pages/Dashboard/Technologies/index.jsx
+++ src/pages/Dashboard/Technologies/index.jsx
@@ -3,13 +3,17 @@ import { TitleStyled } from "../../../components/fonts/Title/styles";
 import { ButtonTerciary } from "../../../components/Button/Terciary";
 import plus from "../../../assets/imgs/plus.svg";
 import { CardTech } from "./CardTech";
+import { TechContext } from "../../../providers/TechContext";
+import { useContext } from "react";
 
 export function Technologies({userData}) {
+  const {modal, showModal} = useContext(TechContext);
+
   return (
     <>
       <TechHeaderStyled>
         <TitleStyled tag="h2">Tecnologias</TitleStyled>
-        <ButtonTerciary button>
+        <ButtonTerciary button onClick={() => showModal(true)}>
           <img src={plus} alt="ícone de adição" />
         </ButtonTerciary>
       </TechHeaderStyled>
